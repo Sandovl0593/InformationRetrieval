ebc2e2160b7dd52c6a63dfe0e0438536b0438fbb
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2024-06-18 09:55:04 -0500

    add interaction

--- api/routes.py
+++ api/routes.py
@@ -1,6 +1,7 @@
 from api import app
 from flask import render_template, jsonify, request
 from libs.invertedIndex import InvertedIndex
+import psycopg2 as ps
 
 indexfile = InvertedIndex("index_spotify_songs.json")
 
@@ -9,36 +10,56 @@ def index():
     return render_template('index.html')
 
 
-# send data from txt file in format index inverted (dictionary mapping of pair (token, index_line from csv)) to frontend
-@app.get('/api/query')
+@app.post('/api/query/manual')
 def inverted_index():
-    # asumiento que son array de tokens en una query
-    query = request.args.get('query')
-    k = request.args.get('topK')
+    # from json request get query and topK
+    query = request.json["query"]
 
-    with open("spotify_songs_procesado.csv", encoding="utf-8", mode="r") as csv_file:
+    with open("./dataset/spotify_songs_reduced.csv", encoding="utf-8", mode="r") as csv_file:
         lines = csv_file.readlines()
         size = len(lines[0].split("@")) - 4
 
-        # create index from csv only list of celds of position 3 (lyrics)
-        # indexfile.ensure_index([line.split("@")[3] for line in lines[1:]])
-        result = indexfile.retrieval(query, k)
+    if not query:
+        split_lines = [el.split("@")[:size] for el in lines[1:]]
+        return jsonify(split_lines)
+    else:
+        k = int(request.json["topK"])
         
-        # send each row coindicent with index from inverted index filtered by token from params
+        # por ahora los lyrics no estan preprocesados
+        getLyrics = [line.split("@")[3] for line in lines[1:]]
+
+        indexfile.build_index(getLyrics)
+        result = indexfile.retrieval(query, k)
+
         indexes = []
         for doc_id, _ in result:
             if int(doc_id) not in indexes:
                 indexes.append(doc_id)
     
         getLines = [lines[int(index)].split("@")[:size] for index in indexes]
-    return jsonify(getLines)
+        return jsonify(getLines)
 
-@app.get('/api/csv')
-def csv():
-    with open("./dataset/spotify_songs_reduced.csv", encoding="utf-8", mode="r") as csv_file:
-        lines = csv_file.readlines()
-        size = len(lines[0].split("@")) - 4
+@app.post('/api/query/postgres')
+def postgres():
+    # from json request get query and topK
+    query = request.json["query"]
+    k = request.json["topK"]
+    print(k, query)
 
-        split_lines = [el.split("@")[:size] for el in lines[1:]]
-   
-    return jsonify(split_lines)
+    conn = ps.connect(
+        # connection configuration
+    )
+    cur = conn.cursor()
+
+    # execute query
+    if not query:
+        cur.execute("SELECT * FROM songs LIMIT %s", (k,))
+    else:
+        cur.execute("SELECT * FROM songs WHERE lyrics @@ to_tsquery(%s) LIMIT %s", (query, k))
+    result = cur.fetchall()
+
+    # close connection
+    cur.close()
+    conn.close()
+
+    return jsonify(result)
--- api/static/index.js
+++ api/static/index.js
@@ -1,54 +1,83 @@
 // Jquery be like
 const $ = (selector) => document.querySelector(selector);
+const $id = (id) => document.getElementById(id);
 
-function setupTopK() {
-  const topKValue = $("#topK-value");
-  const inpTopK = $("#inp-topK");
-  inpTopK.addEventListener("input", () => {
-    topKValue.innerHTML = inpTopK.value;
-  });
+function displayLoadingModal() {
+  const modal = $(".modal-load");
+  // setTimeout(() => {
+  //   modal.style.display = "block";
+  // }, 1000);
+  modal.style.display = "block";
+}
+
+function hideLoadingModal() {
+  const modal = $(".modal-load");
+  modal.style.display = "none";
+}
+
+function loadLyrics(line, key) {
+  const modal = $(".modal");
+  const title = $("#modal-title");
+  const modalText = $("#modal-text");
+  modalText.textContent = line[key];
+  title.textContent = line[1];
+  modal.style.display = "block";
+  const span = $(".close");
+  span.onclick = () => {
+    modal.style.display = "none";
+  };
+  window.onclick = (event) => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+
+function createHeader() {
+  const headers = [
+    "Track id","Track name","Track artist","Lyrics","Track popularity",
+    "Track album id","Track album name","Track album release date","Playlist name",
+    "Playlist id","Playlist genre","Playlist subgenre","Danceability","Energy","Key",
+    "Loudness","Mode","Speechiness","Acousticness","Instrumentalness","Liveness",
+    "Valence","Tempo","Duration (ms)","Language"
+  ];  
+  const head = $("thead");
+  head.innerHTML = headers.map(header => `<th>${header}</th>`).join('');
 }
 
 // fetch lines from the server and display a row for each one
-async function fetchLines() {
-  // const response = await fetch("/api/query");
-  const response = await fetch("/api/csv");
-  const lines = await response.json();
-  const tbody = $("tbody");
-  tbody.innerHTML = "";
-  lines.forEach((line) => {
-    const tr = document.createElement("tr");
-    for (const key in line) {
-      const td = document.createElement("td");
-      if (key !== "3") {
-        td.textContent = line[key];
-      } else {
-        const buttonLyrics = document.createElement("button");
-        buttonLyrics.textContent = "Mostrar letra";
-        buttonLyrics.addEventListener("click", () => {
-          const modal = $(".modal");
-          const title = $("#modal-title");
-          const modalText = $("#modal-text");
-          modalText.textContent = line[key];
-          title.textContent = line[1];
-          modal.style.display = "block";
-          const span = $(".close");
-          span.onclick = function () {
-            modal.style.display = "none";
-          };
-          window.onclick = function (event) {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          };
-        });
-        td.appendChild(buttonLyrics);
+function fetchLines() {
+  displayLoadingModal();
+  const consulta = $id("inp-consulta").value;
+  const tecnica = $id("inp-tecnica").value;
+  fetch(`/api/query/${tecnica}`, {
+    method: "POST", headers: { "Content-Type": "application/json",},
+    body: JSON.stringify({
+      query: consulta,
+      topK: $id("inp-topK").value,
+    }),
+  })
+  .then((response) => response.json())
+  .then(json => {
+    hideLoadingModal();
+    const tbody = $("tbody");
+    tbody.innerHTML = "";
+    createHeader();
+    json.forEach((line) => {
+      const tr = document.createElement("tr");
+      for (const key in line) {
+        const td = document.createElement("td");
+        if (key !== "3") {
+          td.textContent = line[key];
+        } else {
+          const buttonLyrics = document.createElement("button");
+          buttonLyrics.textContent = "Mostrar letra";
+          buttonLyrics.addEventListener("click", () => {loadLyrics(line, key); });
+          td.appendChild(buttonLyrics);
+        }
+        tr.appendChild(td);
       }
-      tr.appendChild(td);
-    }
-    tbody.appendChild(tr);
-  });
+      tbody.appendChild(tr);
+    });
+  })
 }
-
-
-setupTopK();
