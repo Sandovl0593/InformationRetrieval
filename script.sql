20d1bbaa20443aee623a9ba332ada308bddc4238
FernandoGuillenR <66321787+FernandoGuillenR@users.noreply.github.com>
2024-06-19 19:27:23 -0500

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,5 +1,5 @@
 -- Crear la tabla
-CREATE TABLE tracks (
+CREATE TABLE songs (
     track_id VARCHAR,
     track_name VARCHAR,
     track_artist VARCHAR,
@@ -28,29 +28,28 @@ CREATE TABLE tracks (
     text TEXT,
     tokens TEXT,
     filtered_tokens TEXT,
-    stemmed_tokens TEXT,
-    tsvector_col tsvector  -- Columna para índice de texto completo
+    stemmed_tokens TEXT
 );
 
 -- Cargar datos desde CSV con delimitador '@'
-COPY tracks FROM '/Users/fernandoguill3n/data.csv' DELIMITER '@' CSV HEADER;
+COPY songs FROM '/Users/fernandoguill3n/data.csv' DELIMITER '@' CSV HEADER;
 
 -- Crear extensiones necesarias si no están instaladas
 CREATE EXTENSION IF NOT EXISTS pg_trgm;  -- Extension para trigramas
 CREATE EXTENSION IF NOT EXISTS unaccent; -- Extension para remover acentos
 
 -- Crear la columna tsvector
-ALTER TABLE tracks ADD COLUMN tsvector_col tsvector;
+ALTER TABLE songs ADD COLUMN tsvector_col tsvector;
 
 -- Actualizar la columna tsvector con el texto procesado
-UPDATE tracks SET tsvector_col = to_tsvector('spanish', unaccent(text));
+UPDATE songs SET tsvector_col = to_tsvector('spanish', unaccent(text));
 
 -- Crear el índice GIN en tsvector_col para optimizar búsquedas de texto completo
-CREATE INDEX idx_tsvector ON tracks USING GIN(tsvector_col);
+CREATE INDEX idx_tsvector ON songs USING GIN(tsvector_col);
 
 -- Consulta con similitud de coseno
 SELECT track_id, track_name, track_artist,
        ts_rank_cd(tsvector_col, query) AS rank
-FROM tracks, to_tsquery('spanish', 'consulta de ejemplo') AS query
+FROM songs, to_tsquery('spanish', 'consulta de ejemplo') AS query
 WHERE tsvector_col @@ query
 ORDER BY rank DESC;
