c48f9d3f2ce04d90326d76c1d638fd25b9ce9d5f
FernandoGuillenR <66321787+FernandoGuillenR@users.noreply.github.com>
2024-07-18 20:48:18 -0500

    Update knnHighD.py
    Upgraded with defaultdict library

--- libs/knnHighD.py
+++ libs/knnHighD.py
@@ -1,6 +1,8 @@
 import numpy as np
 import re
 import ast
+from collections import defaultdict
+from scipy.spatial.distance import euclidean
 
 # Función hash basada en proyecciones aleatorias
 def hash_function(vector, random_vectors):
@@ -9,13 +11,11 @@ def hash_function(vector, random_vectors):
 # Crear el índice LSH
 def create_lsh_index(data, num_hash_functions, vector_size):
     random_vectors = [np.random.randn(vector_size) for _ in range(num_hash_functions)]
-    lsh_index = {}
+    lsh_index = defaultdict(list)
     
     for index, row in data.iterrows():
         mfcc_vector = row['MFCC_Vector']
         hash_value = hash_function(mfcc_vector, random_vectors)
-        if hash_value not in lsh_index:
-            lsh_index[hash_value] = []
         lsh_index[hash_value].append((index, mfcc_vector))
     
     return lsh_index, random_vectors
@@ -25,15 +25,10 @@ def lsh_knn_search(query_vector, lsh_index, random_vectors, K):
     query_hash = hash_function(query_vector, random_vectors)
     candidate_vectors = lsh_index.get(query_hash, [])
     
-    # Si no se encuentran candidatos, se puede buscar en hashes vecinos
     if len(candidate_vectors) == 0:
         return []
 
-    distances = []
-    for index, vector in candidate_vectors:
-        distance = np.linalg.norm(query_vector - vector)
-        distances.append((distance, index))
-    
+    distances = [(euclidean(query_vector, vector), index) for index, vector in candidate_vectors]
     distances.sort()
     return distances[:K]
 
